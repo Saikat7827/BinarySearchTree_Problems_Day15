b457bb8a62c7acc85239530f691195330a7656d2
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-12-01 08:14:25 +0530

    Added:Element Inserted

--- BST.java
+++ BST.java
@@ -1,7 +1,59 @@
 package com.bridgelabz;
 
-public class BST {
-public static void main(String[] args) {
-	System.out.println("Welcome to binary Search Tree Program");
+
+class Node {
+	int data;
+	Node left;
+	Node right;
+
+	public Node(int data) {
+		this.data = data;
+		Node left = null;
+		Node right = null;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + ", left=" + left + ", right=" + right + "]";
+	}
+
 }
+
+/*
+ * @Purpose : Insert Element in BST
+ * 
+ * @Param : Node,Data
+ * 
+ * @return : Node
+ */
+public class BST {
+	public Node insert(Node node, int data) {
+
+		if (node == null) {
+			node = createNewNode(data);
+		}
+
+		if (data < node.data) {
+			node.left = insert(node.left, data);
+		} else if (data > node.data) {
+			node.right = insert(node.right, data);
+		}
+		return node;
+	}
+
+	private Node createNewNode(int data) {
+		Node mynode = new Node(data);
+		return mynode;
+	}
+
+	public void printInorder(Node node) {
+		if (node == null) {
+			return;
+		}
+		printInorder(node.left);
+		System.out.print(node.data + "->");
+		printInorder(node.right);
+
+	}
+
 }
